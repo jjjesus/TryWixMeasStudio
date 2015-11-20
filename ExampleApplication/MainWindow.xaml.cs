2a019f64a26e78c2ba0abfc1887e08097905d083
John Jesus <john.jesus@raytheon.com>
2015-11-19 21:23:07 -0600

    Successful installer build.

---
+++ ExampleApplication/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using NationalInstruments.Analysis;
+using NationalInstruments.Analysis.Conversion;
+using NationalInstruments.Analysis.Dsp;
+using NationalInstruments.Analysis.Dsp.Filters;
+using NationalInstruments.Analysis.Math;
+using NationalInstruments.Analysis.Monitoring;
+using NationalInstruments.Analysis.SignalGeneration;
+using NationalInstruments.Analysis.SpectralMeasurements;
+using NationalInstruments;
+using NationalInstruments.Controls;
+using NationalInstruments.Controls.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ExampleApplication
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
